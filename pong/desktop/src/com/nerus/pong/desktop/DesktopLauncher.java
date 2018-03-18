c0c3b56c06615b27bf20ecdead2d799e46e628bc
Nerus92 <jphilippe.chatain@gmail.com>
2018-03-18 14:52:28 -0700

    Rendering the bouncing ball

--- pong/desktop/src/com/nerus/pong/desktop/DesktopLauncher.java
+++ pong/desktop/src/com/nerus/pong/desktop/DesktopLauncher.java
@@ -2,11 +2,15 @@ package com.nerus.pong.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nerus.pong.Pong;
 import com.nerus.pong.PongGDX;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+		Pong pong = new Pong();
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new PongGDX(), config);
+        config.width = 512;
+        config.height = 256;
+		new LwjglApplication(new PongGDX(pong), config);
 	}
 }
