5846ea5d60e7a535c6aac4b7b1ce783b4f4de611
Nerus92 <jphilippe.chatain@gmail.com>
2018-03-17 17:37:00 -0700

    Converting to LibGDX

---
+++ pong/desktop/src/com/nerus/pong/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nerus.pong.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nerus.pong.PongGDX;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PongGDX(), config);
+	}
+}
